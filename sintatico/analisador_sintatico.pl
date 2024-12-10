3dc54049c0c3e3b53ac676ad5d8c61ebe5bd25ab
Bruno Martins <bruno.martval@gmail.com>
2024-12-10 01:15:27 -0300

    add comentários com regras de produção para facilitar o entendimento do código

--- sintatico/analisador_sintatico.pl
+++ sintatico/analisador_sintatico.pl
@@ -19,6 +19,8 @@ optionalToken(Token, [Lookahead|List], Return) :-
     
     Return = [Lookahead|List].
 
+
+% <program> ::= <typeSpec> main ( [void] ) <compoundStmt>
 program(Tokens0, Return) :-
     typeSpec(Tokens0, Tokens1), 
     assertToken(main, Tokens1, Tokens2),
@@ -28,6 +30,7 @@ program(Tokens0, Return) :-
     compoundStmt(Tokens5, Tokens6),
     Return = Tokens6.
 
+% <typeSpec> ::= void | char | int
 typeSpec(Tokens0, Return) :-
     assertToken(void, Tokens0, Tokens1), 
     Return = Tokens1, 
@@ -41,6 +44,7 @@ typeSpec(Tokens0, Return) :-
     Return = Tokens3,
     !.
 
+% <compoundStmt> ::= { <localDecls> <stmtList> }
 compoundStmt(Tokens0, Return) :- 
     assertToken(left_brace, Tokens0, Tokens1),
     localDecls(Tokens1, Tokens2),
@@ -48,7 +52,7 @@ compoundStmt(Tokens0, Return) :-
     assertToken(right_brace, Tokens2, Tokens4),
     Return = Tokens4.
 
-
+% <localDecls> ::= <scopedVarDecl> <localDecls_> | ε
 localDecls(Tokens0, Return) :- 
     scopedVarDecl(Tokens0, Tokens1),
     localDecls_(Tokens1, Tokens2),
@@ -56,6 +60,7 @@ localDecls(Tokens0, Return) :-
 
     Return = Tokens0.
 
+% <localDecls_> ::= <scopedVarDecl> <localDecls_> | ε
 localDecls_(Tokens0, Return) :-
     scopedVarDecl(Tokens0, Tokens1),
     localDecls_(Tokens1, Tokens2),
@@ -63,17 +68,20 @@ localDecls_(Tokens0, Return) :-
 
     Return = Tokens0.
 
+% <scopedVarDecl> ::= <typeSpec> <varDeclList> ;
 scopedVarDecl(Tokens0, Return) :- 
     typeSpec(Tokens0, Tokens1),
     varDecList(Tokens1, Tokens2),
     assertToken(semicolon, Tokens2, Tokens3),
     Return = Tokens3.
 
+% <varDeclList> ::= <varDeclInit> <varDecList_>
 varDecList(Tokens0, Return) :-
     varDeclInit(Tokens0, Tokens1),
     varDecList_(Tokens1, Tokens2),
     Return = Tokens2.
 
+% <varDeclList_> ::= , <varDeclInit> <varDeclList_> | ε
 varDecList_(Tokens0, Return) :-
     assertToken(comma, Tokens0, Tokens1),
     varDeclInit(Tokens1, Tokens2),
@@ -82,6 +90,7 @@ varDecList_(Tokens0, Return) :-
 
     Return = Tokens0.
 
+% <varDeclInit> ::= <varDeclId> | <varDeclId> = <simpleExp>
 varDeclInit(Tokens0, Return) :-
     varDeclId(Tokens0, Tokens1),
     Return = Tokens1.
@@ -91,10 +100,12 @@ varDeclInit(Tokens0, Return) :-
     simpleExp(Tokens2, Tokens3),
     Return = Tokens3.
 
+% <varDeclId> ::= ID
 varDeclId(Tokens0, Return) :-
     assertId(Tokens0, Tokens1),
     Return = Tokens1.
 
+% <stmtList> ::= <stmt> <stmtList_> | ε
 stmtList(Tokens0, Return) :-
     stmt(Tokens0, Tokens1),
     stmtList_(Tokens1, Tokens2),
@@ -102,6 +113,7 @@ stmtList(Tokens0, Return) :-
 
     Return = Tokens0.
 
+% <stmtList_> ::= <stmt> <stmtList_> | ε
 stmtList_(Tokens1, Return) :-
     stmt(Tokens0, Tokens1),
     stmtList_(Tokens1, Tokens2),
@@ -109,6 +121,7 @@ stmtList_(Tokens1, Return) :-
 
     Return = Tokens0.
 
+% <stmt> ::= <expStmt> | <compoundStmt> | <selectStmt> | <iterStmt> | <returnStmt> | <breakStmt>
 stmt(Tokens0, Return) :-
     expStmt(Tokens0, Tokens1),
     Return = Tokens1;
@@ -128,6 +141,7 @@ stmt(Tokens0, Return) :-
     breakStmt(Tokens0, Tokens1),
     Return = Tokens1.
 
+% <expStmt> ::= <exp> ; | ;
 expStmt(Tokens0, Return) :- 
     exp(Tokens0, Tokens1), 
     assertToken(semicolon, Tokens1, Tokens2),
@@ -136,6 +150,7 @@ expStmt(Tokens0, Return) :-
     assertToken(semicolon, Tokens0, Tokens1),
     Return = Tokens1.
 
+% <selectStmt> ::= if <simpleExp> <stmt> | if <simpleExp> <stmt> else <stmt>
 selectStmt(Tokens0, Return) :-
     assertToken(if, Tokens0, Tokens1),
     simpleExp(Tokens1, Tokens2),
@@ -149,12 +164,14 @@ selectStmt(Tokens0, Return) :-
     stmt(Tokens4, Tokens5),
     Return = Tokens5.
 
+% <iterStmt> ::= while <simpleExp> <stmt>
 iterStmt(Tokens0, Return) :-
     assertToken(while, Tokens0, Tokens1),
     simpleExp(Tokens1, Tokens2),
     stmt(Tokens2, Tokens3),
     Return = Tokens3.
 
+% <returnStmt> ::= return ; | return <exp> ;
 returnStmt(Tokens0, Return) :-
     assertToken(return, Tokens0, Tokens1),
     assertToken(semicolon, Tokens1, Tokens2),
@@ -165,11 +182,13 @@ returnStmt(Tokens0, Return) :-
     assertToken(semicolon, Tokens2, Tokens3),
     Return = Tokens3.
 
+% <breakStmt> ::= break ;
 breakStmt(Tokens0, Return) :-
     assertToken(break, Tokens0, Tokens1),
     assertToken(semicolon, Tokens1, Tokens2),
     Return = Tokens2.
 
+% <exp> ::= <mutable> <assignop> <exp> | <mutable>++ | <mutable>-- | <simpleExp>
 exp(Tokens0, Return) :-
     mutable(Tokens0, Tokens1),
     assignop(Tokens1, Tokens2),
@@ -187,6 +206,7 @@ exp(Tokens0, Return) :-
     simpleExp(Tokens0, Tokens1),
     Return = Tokens1.
 
+% <assignop> ::=  = | += | -= | *= | /=/
 assignop(Tokens0, Return) :-
     assertToken(assign, Tokens0, Tokens1),
 	Return = Tokens1;
@@ -203,12 +223,13 @@ assignop(Tokens0, Return) :-
 	assertToken(div_assign, Tokens0, Tokens1),
 	Return = Tokens1.
 
-
+% <simpleExp> ::= <andExp> <simpleExp_>
 simpleExp(Tokens0, Return) :-
 	andExp(Tokens0, Tokens1),
 	simpleExp_(Tokens1, Tokens2),
 	Return = Tokens2.
-	
+
+% <simpleExp_> ::= <or> <andExp> <simpleExp_> | ε
 simpleExp_(Tokens0, Return) :-
 	assertToken(or, Tokens0, Tokens1),
 	andExp(Tokens1, Tokens2),
@@ -217,11 +238,13 @@ simpleExp_(Tokens0, Return) :-
 
 	Return = Tokens0.
 
+% <andExp> ::= <unaryRelExp> <andExp_>
 andExp(Tokens0, Return) :-
 	unaryRelExp(Tokens0, Tokens1),
 	andExp_(Tokens1, Tokens2),
 	Return = Tokens2.
 
+% <andExp_> ::= <and> <unaryRelExp> <andExp_> | ε
 andExp_(Tokens0, Return) :-
 	assertToken(and, Tokens0, Tokens1),
 	unaryRelExp(Tokens1, Tokens2),
@@ -230,6 +253,7 @@ andExp_(Tokens0, Return) :-
 
 	Return = Tokens0.
 
+% <unaryRelExp> ::= <not> <unaryRelExp> | <relExp>
 unaryRelExp(Tokens0, Return) :-
 	assertToken(not, Tokens0, Tokens1),
 	unaryRelExp(Tokens1, Tokens2),
@@ -238,6 +262,7 @@ unaryRelExp(Tokens0, Return) :-
 	relExp(Tokens0, Tokens1),
 	Return = Tokens1.
 
+% <relExp> ::= <sumExp> <relop> <sumExp> | <sumExp>
 relExp(Tokens0, Return) :-
 	sumExp(Tokens0, Tokens1),
 	relop(Tokens1, Tokens2),
@@ -247,6 +272,7 @@ relExp(Tokens0, Return) :-
 	sumExp(Tokens0, Tokens1),
 	Return = Tokens1.
 
+% <relop> ::= < | <= | > | >= | == | !=
 relop(Tokens0, Return) :-
 	assertToken(relop('<'), Tokens0, Tokens1),
 	Return = Tokens1;
@@ -266,11 +292,13 @@ relop(Tokens0, Return) :-
 	assertToken(relop('!='), Tokens0, Tokens1),
 	Return = Tokens1.
 
+% <sumExp> ::= <mulExp> < sumExp_>
 sumExp(Tokens0, Return) :-
 	mulExp(Tokens0, Tokens1),
 	sumExp_(Tokens1, Tokens2);
 	Return = Tokens2.
 
+% <sumExp_> ::= <sumop> <mulExp> <sumExp_> | ε
 sumExp_(Tokens0, Return) :-
 	sumop(Tokens0, Tokens1),
 	mulExp(Tokens1, Tokens2),
@@ -279,16 +307,19 @@ sumExp_(Tokens0, Return) :-
 
 	Return = Tokens0.
 
+% <sumop> ::= + | -
 sumop(Tokens0, Return) :-
     assertToken(sumop('+'), Tokens0, Tokens1),
     Return = Tokens1;
     assertToken(sumop('-'), Tokens0, Tokens1),
     Return = Tokens1.
 
+% <mulExp> ::= <unaryExp> <mulExp_>
 mulExp(Tokens0, Return) :-
     unaryExp(Tokens0, Tokens1),
     mulExp_(Tokens1, Return).
 
+% <mulExp_> ::= <mulop> <unaryExp> <mulExp_> | ε
 mulExp_(Tokens0, Return) :-
     mulop(Tokens0, Tokens1),
     unaryExp(Tokens1, Tokens2),
@@ -296,6 +327,7 @@ mulExp_(Tokens0, Return) :-
     Return = Tokens3;
     Return = Tokens0.
 
+% <mulop> ::= * | / | %/
 mulop(Tokens0, Return) :-
     assertToken(mulop('*'), Tokens0, Tokens1),
     Return = Tokens1;
@@ -304,38 +336,48 @@ mulop(Tokens0, Return) :-
     assertToken(mulop('%'), Tokens0, Tokens1),
     Return = Tokens1.
 
+% <unaryExp> ::= <unaryop> <unaryExp> | <factor>
 unaryExp(Tokens0, Return) :-
     unaryop(Tokens0, Tokens1),
     unaryExp(Tokens1, Return);
     factor(Tokens0, Return).
 
+% <unaryop> ::= -
 unaryop(Tokens0, Return) :-
     assertToken(unaryop('-'), Tokens0, Return).
 
+
+% <factor> ::= <mutable> | <immutable>
 factor(Tokens0, Return) :-
     mutable(Tokens0, Return);
     immutable(Tokens0, Return).
 
+% <mutable> ::= ID
 mutable(Tokens0, Return) :-
     assertId(Tokens0, Return).
 
+% <immutable> ::= ( <exp> ) | <constant>
 immutable(Tokens0, Return) :-
     assertToken(left_paren, Tokens0, Tokens1),
     exp(Tokens1, Tokens2),
     assertToken(right_paren, Tokens2, Return);
     constant(Tokens0, Return).
 
+% <constant> ::= NUMCONST | CHARCONST | true | false
 constant(Tokens0, Return) :-
     assertToken(numconst, Tokens0, Return);
     assertToken(charconst, Tokens0, Return);
     assertToken(true, Tokens0, Return);
     assertToken(false, Tokens0, Return).
 
+% <and> ::= &&
 and(Tokens0, Return) :-
     assertToken(and, Tokens0, Return).
 
+% <or> ::= ||
 or(Tokens0, Return) :-
     assertToken(or, Tokens0, Return).
 
+% <not> ::= !
 not(Tokens0, Return) :-
     assertToken(not, Tokens0, Return).
